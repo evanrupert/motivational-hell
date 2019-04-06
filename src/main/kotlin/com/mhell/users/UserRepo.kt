1e6227848b0710b66e29ca910ef60d8bd319eae4
Evan Rupert <rupertevanr@gmail.com>
2019-04-06 15:05:10 -0400

    Added CRUD operations to UserRepo

--- src/main/kotlin/com/mhell/users/UserRepo.kt
+++ src/main/kotlin/com/mhell/users/UserRepo.kt
@@ -1,9 +1,8 @@
 package com.mhell.users
 
 import com.mhell.Repo
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import java.util.*
 
 object UserRepo : Repo() {
 
@@ -24,5 +23,30 @@ object UserRepo : Repo() {
 
     fun all(): List<User> = query {
         Users.selectAll().map(Users::toUser)
+    }
+
+    fun find(id: UUID): User = query {
+        Users.select { Users.id eq id }.map(Users::toUser).first()
+    }
+
+    fun create(user: User) = query {
+        Users.insert {
+            it[id] = user.id
+            it[email] = user.email
+            it[password] = user.password
+            it[balance] = user.balance
+        }
+    }
+
+    fun update(user: User) = query {
+        Users.update({ Users.id eq user.id }) {
+            it[email] = user.email
+            it[password] = user.password
+            it[balance] = user.balance
+        }
+    }
+
+    fun delete(id: UUID) = query {
+        Users.deleteWhere { Users.id eq id }
     }
 }
